91f8a5c72f61872864b8d2f126b05dd6e2196f31
tth010299 <tth010299@gmail.com>
2020-06-04 00:01:39 +0700

    [thanhhai] Update connection string.

--- ssrcore/Models/ApplicationDbContext.cs
+++ ssrcore/Models/ApplicationDbContext.cs
@@ -30,7 +30,7 @@ namespace ssrcore.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=SE130022\\SQLEXPRESS;Database=SSR_DB;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer("Server=tcp:se130022.database.windows.net,1433;Initial Catalog=SSR_DB;Persist Security Info=False;User ID=thanhhai;Password=Petronas@217;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
             }
         }
 

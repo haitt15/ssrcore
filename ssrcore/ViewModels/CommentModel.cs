81f516545b4a9e68518bdeabf453fb8eed7abc10
Nguyen The Son <sh2812995@gmail.com>
2020-07-20 22:45:10 +0700

    [SonNTSE130084] hot fix comment

--- ssrcore/ViewModels/CommentModel.cs
+++ ssrcore/ViewModels/CommentModel.cs
@@ -10,7 +10,6 @@ namespace ssrcore.ViewModels
         public int Id { get; set; }
         public string TicketId { get; set; }
         public string Content { get; set; }
-        public int UserId { get; set; }
         public string Username { get; set; }
         public string FullName { get; set; }
     }

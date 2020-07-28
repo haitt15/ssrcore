9f69a435442565aef85f62fdf47a7a0c5c811c59
Nguyen The Son <sh2812995@gmail.com>
2020-07-28 11:20:34 +0700

    [SonNTSE130084] fixbug substring service list

--- client-ssrcore/src/views/Service.vue
+++ client-ssrcore/src/views/Service.vue
@@ -72,7 +72,7 @@ export default {
   },
   filters: {
     subStringValue (value) {
-      if (value.length > 30 && value) {
+      if (value && value.length > 30) {
         value = value.substring(0, 30) + '...'
       }
       return value
